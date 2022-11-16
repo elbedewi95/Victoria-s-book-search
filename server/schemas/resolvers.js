50ffba34745453dbae2324071016fbde7bfb9c22
elbedewi95 <vickybedewi-95@hotmail.com>
2022-11-15 17:12:11 -0800

    save book

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -32,6 +32,17 @@ const resolvers = {
             const token = signToken(user);
             return { token, user };
         },
+        saveBook: async (parent, { book }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: {savedBooks: book} },
+                    { new: true }
+                )
+                return updatedUser;
+            }
+            throw new AuthenticationError('You need to be logged in!')
+        },
     }
   };
   
