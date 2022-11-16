676aced31de7b21e7d275f715bbf3cb3082fc3b5
elbedewi95 <vickybedewi-95@hotmail.com>
2022-11-15 17:11:37 -0800

    addUser

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -13,6 +13,14 @@ const resolvers = {
             throw new AuthenticationError('Not logged in');
         }
     },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+    }
   };
   
   module.exports = resolvers;
