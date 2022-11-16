e275627eae427c766b838db6f51b40f33039afbd
elbedewi95 <vickybedewi-95@hotmail.com>
2022-11-15 17:17:13 -0800

    import auth middleware

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 // import typeDefs and resolvers
 const { typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 //const routes = require('./routes');
 
