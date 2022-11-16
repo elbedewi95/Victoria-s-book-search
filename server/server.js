cad34d1d04ee327f9ca602c2c81ca8b001bca0e2
elbedewi95 <vickybedewi-95@hotmail.com>
2022-11-15 17:16:22 -0800

    import resolvers and type defs

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 //import apollo server
 const { ApolloServer } = require('apollo-server-express');
+// import typeDefs and resolvers
+const { typeDefs, resolvers} = require('./schemas');
 const db = require('./config/connection');
 const routes = require('./routes');
 
